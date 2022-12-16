b94c7fda80eba574ebe3d6bdbdfc761a426a939c
Akexander Dorofyuchik <derafwork@gmail.com>
2022-12-16 10:58:52 +0300

    add funk Theme

--- src/shared/header/Header.tsx
+++ src/shared/header/Header.tsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Select from "react-select";
 import GlobalSwgSelector from "../../assets/icons/GlobalSwgSelector";
+import { Theme } from "../../context/ThemeContext";
+import { useTheme } from "../../hooks/useTheme";
 import s from "./Header.module.scss";
 type Props = {};
 
 export const Header = (props: Props) => {
+  const theme = useTheme();
+
   const options = [
     { value: "city-1", label: "Санкт-Петербург" },
     { value: "city-2", label: "Москва" },
@@ -14,14 +18,23 @@ export const Header = (props: Props) => {
   const colorStyle = {
     control: (styles: any) => ({
       ...styles,
-      backgroundColor: "rgba(21,147,255,0.2)",
+      backgroundColor:
+        theme.theme === Theme.DARK ? "#4f4f4f" : "rgba(21,147,255,0.2)",
       width: "194px",
       height: "37px",
       border: "none",
       borderRadius: "10px",
       zIndex: 100,
     }),
+    singleValue: (styles: any) => ({
+      ...styles,
+      color: theme.theme === Theme.DARK ? "#fff" : "#000",
+    }),
   };
+  const changeTheme = () => {
+    theme.changeTheme(theme.theme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT);
+  };
+
   return (
     <header className={s.header}>
       <div className={s.wrapper}>
@@ -31,7 +44,7 @@ export const Header = (props: Props) => {
         <div className={s.title}>React weather</div>
       </div>
       <div className={s.wrapper}>
-        <div className={s.change_them}>
+        <div className={s.change_them} onClick={changeTheme}>
           <GlobalSwgSelector id='change-them' />
         </div>
         <Select
